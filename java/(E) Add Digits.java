57d241ebc22f19d87026d91b233230e8f7834e03
dipu2015 <dan.umass@gmail.com>
2015-12-18 23:56:55 -0800

    Create (E) Add Digits.java

---
+++ java/(E) Add Digits.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int addDigits(int num) {
+        int s = 0;
+        while(num > 0) {
+            s += num % 10;
+            num = num / 10;
+        }
+        if (s < 10)
+            return s;
+        else
+            return addDigits(s);
+    }
+}
