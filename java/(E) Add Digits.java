eeaa736c13394a90ca888f9d376a2dc3c92755d2
dipu2015 <dan.umass@gmail.com>
2015-12-18 23:59:32 -0800

    Update (E) Add Digits.java

--- java/(E) Add Digits.java
+++ java/(E) Add Digits.java
@@ -3,7 +3,7 @@ public class Solution {
         int s = 0;
         while(num > 0) {
             s += num % 10;
-            num = num / 10;
+            num /= 10;
         }
         if (s < 10)
             return s;
