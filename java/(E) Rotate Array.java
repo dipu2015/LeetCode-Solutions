e64a4802d5ee6e3d9d8b9a95097c1f919b9952e9
dipu2015 <dan.umass@gmail.com>
2015-12-25 18:27:43 -0800

    Create (E) Rotate Array.java

---
+++ java/(E) Rotate Array.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n-k-1);
+        reverse(nums, n-k, n-1);
+        reverse(nums, 0, n-1);
+    }
+    
+    private void reverse(int[] nums, int start, int end) {
+        while(start < end) {
+            nums[start] = nums[start] ^ nums[end];
+            nums[end] = nums[end] ^ nums[start];
+            nums[start] = nums[start] ^ nums[end];
+            start++;
+            end--;
+        }
+    }
+}
