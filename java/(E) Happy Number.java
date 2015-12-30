f0f58b93b2891ab8357b5e8d47edf374b92f3698
dipu2015 <dan.umass@gmail.com>
2015-12-29 18:11:03 -0800

    Create (E) Happy Number.java

---
+++ java/(E) Happy Number.java
@@ -0,0 +1,32 @@
+/*
+Write an algorithm to determine if a number is "happy".
+
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+Example: 19 is a happy number
+
+1^2 + 9^2 = 82
+8^2 + 2^2 = 68
+6^2 + 8^2 = 100
+1^2 + 0^2 + 0^2 = 1
+*/
+
+public class Solution {
+    public boolean isHappy(int n) {
+        HashSet<Integer> visited = new HashSet<Integer>();
+        while(n != 1) {
+            if (visited.contains(n))
+                return false;
+            visited.add(n);
+            
+            int sum = 0;
+            while(n > 0) {
+                int res = n % 10;
+                sum += res * res;
+                n /= 10;
+            }
+            n = sum;
+        }
+        return true;
+    }
+}
