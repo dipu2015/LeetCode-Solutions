ec256cc4038e0683a0def785bfed52f5dbc9256b
dipu2015 <dan.umass@gmail.com>
2016-01-23 19:24:13 -0800

    Update (M) Longest Substring Without Repeating Characters.java

--- java/(M) Longest Substring Without Repeating Characters.java
+++ java/(M) Longest Substring Without Repeating Characters.java
@@ -1,30 +1,21 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
         int max = 0;
-        int temp_length = 0;
-        int new_begin = -1;
-        Map<Character, Integer> dict = new HashMap<Character, Integer>(); // Character
+        int start = 0;
         for (int i=0; i<s.length(); i++) {
             char c = s.charAt(i);
-            if (dict.get(c) != null) {
-                int idx = dict.get(c);
-                if (idx >= new_begin) {
-                    temp_length = i - idx;
-                    if (temp_length > max)
-                        max = temp_length;
-                    dict.put(c, i);
-                    new_begin = idx + 1;
-                    continue;
+            if (map.containsKey(c)) {
+                if (map.get(c) >= start) {
+                    max = Math.max(max, i - start);
+                    start = map.get(c) + 1;
                 }
             }
-            dict.put(c, i);
-            temp_length++;
-            if (temp_length > max)
-                max = temp_length;
+            map.put(c, i);
         }
+        int len = s.length() - start;
+        max = Math.max(max, len);
+        
         return max;
     }
 }
